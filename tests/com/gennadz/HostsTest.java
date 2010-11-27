7a9e0d02a11b00e2a2681fd2e78e6529352a7d59
Gennadiy Zlobin <gennad.zlobin@gmail.com>
2010-11-28 03:45:19 +0600

    Tests created

--- tests/com/gennadz/HostsTest.java
+++ tests/com/gennadz/HostsTest.java
@@ -3,6 +3,8 @@
  */
 package com.gennadz;
 
+import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -52,14 +54,22 @@ public class HostsTest extends TestCase{
 	}
 	
 	@Test
-	public void testDeleteTempFile() throws SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+	public void testDeleteTempFile() throws SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, NoSuchFieldException {
 		Hosts h = new Hosts();
 		Class hostsClass = h.getClass();
 		//Class<String> c = String.class;
-		Method method = hostsClass.getDeclaredMethod("deleteTempFile", null);
+		Method method = hostsClass.getDeclaredMethod("deleteFile", String.class);
 		method.setAccessible(true);
 		
-		Boolean result =  (Boolean)method.invoke(h);
+		Field fieldName = hostsClass.getDeclaredField("tempFileName");
+		fieldName.setAccessible(true);
+		String tempFileName = (String) fieldName.get(h);
+		Field fieldFile = hostsClass.getDeclaredField("tempFile");
+		fieldFile.setAccessible(true);
+		File fileTempFile = (File) fieldFile.get(h);
+		fileTempFile = new File(tempFileName);
+		
+		Boolean result =  (Boolean)method.invoke(h, fileTempFile);
 		Boolean t = new Boolean(true); //hack
 	    assertEquals(t, result);
 	}
@@ -67,5 +77,47 @@ public class HostsTest extends TestCase{
 	public void testGetAllHosts() {
 		Hosts h = new Hosts();
 		h.getAllHosts();
+	}
+	
+	public void testCleanHost() throws SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		Hosts h = new Hosts();
+		Class hc = h.getClass();
+		Method m = hc.getDeclaredMethod("cleanHost", String.class);
+		m.setAccessible(true);
+		String result =  (String)m.invoke(h, "http://www.yandex.ru");
+		assertEquals("yandex.ru", result);
+		
+		result =  (String)m.invoke(h, "www.gennad.ru");
+		assertEquals("gennad.ru", result);
+		
+		result =  (String)m.invoke(h, "https://www.gennad.ru");
+		assertEquals("gennad.ru", result);
+		
+		result =  (String)m.invoke(h, "svn://www.gennad.ru");
+		assertEquals("gennad.ru", result);
+		
+		result =  (String)m.invoke(h, "http://www.blog.gennad.ru");
+		assertEquals("gennad.ru", result);
+		
+		result =  (String)m.invoke(h, "www.blog.gennad.ru");
+		assertEquals("gennad.ru", result);
+		
+		result =  (String)m.invoke(h, "gennad.ru");
+		assertEquals("gennad.ru", result);
+		
+		result =  (String)m.invoke(h, "");
+		assertEquals("", result);
+		
+		result =  (String)m.invoke(h, "gennad");
+		assertEquals("gennad", result);
+	}
+	
+	public void testAppendHost() throws SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		Hosts h = new Hosts();
+		Class hc = h.getClass();
+		Method m = hc.getDeclaredMethod("appendHost", String.class);
+		m.setAccessible(true);
+		Boolean result =  (Boolean)m.invoke(h, "http://www.yandex.ru");
+		assertEquals(new Boolean(true), result);
 	}
 }
